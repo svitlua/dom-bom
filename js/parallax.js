15cfdf30112a8ec43abb5debbc0940eff93ad7fb
Svitlana S <31032340+svitlua@users.noreply.github.com>
2017-09-07 14:57:30 +0300

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,5 @@
 
 window.addEventListener('scroll', function (e) {
-	// console.log(document.body.scrollTop);
-  //
   var scroll = document.body.scrollTop;
   var parallaxAll = document.getElementsByClassName('parallax');
    for (var i = 0; i<parallaxAll.length; i++){
@@ -9,36 +7,5 @@ window.addEventListener('scroll', function (e) {
     var posY = - (scroll*speed);
     parallaxAll[i].style.backgroundPosition = '0px '+posY+'px';
   }
-
-
 })
 
-
-/*
-(function($){
-  $.fn.parallax = function(options){
-    var $$ = $(this);
-    offset = $$.offset();
-    var defaults = {
-      'start': 0,
-      'stop': offset.top + $$.height(),
-      'coeff': 0.95
-    };
-    var opts = $.extend(defaults, options);
-    return this.each(function(){
-      $(window).bind('scroll', function() {
-        windowTop = $(window).scrollTop();
-        if((windowTop >= opts.start) && (windowTop <= opts.stop)) {
-          newCoord = windowTop * opts.coeff;
-          $$.css({
-              'background-position': '0 '+ newCoord + 'px'
-          });
-        }
-      });
-    });
-  };
-})(jQuery);
-
-// call the plugin
-$('.section').parallax({ 'coeff':-0.65 });
-$('.section .inner').parallax({ 'coeff':1.15 });*/
